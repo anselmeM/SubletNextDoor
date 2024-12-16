07366bef800b6e2364c93bb87c65891518eb07fd
anselme <amotchojr@gmail.com>
2024-12-16 07:09:40 -0500

    Update firebase.js
    commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; // Import Auth service
@@ -6,9 +7,11 @@ import { getFirestore } from "firebase/firestore"; // Import Firestore service
 import dotenv from "dotenv";
 
 // Load environment variables
+
 dotenv.config();
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -20,6 +23,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+ 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app); // Initialize Auth
